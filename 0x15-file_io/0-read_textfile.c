df0838176f9874815338c0e252bcd650c3cb0142
kegera <kegeshaddy@gmail.com>
2023-04-10 17:47:17 -0700

    updates on task 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,51 +1,55 @@
 #include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
- * @filename: A pointer to the name of the file to be read.
- * @letters: The number of letters to read from the file and print.
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters that the function should read and print.
  *
- * Return: The actual number of letters it could read and print.
- *         If the file cannot be opened or read, return 0.
- *         If filename is NULL, return 0.
- *         If write fails or does not write the expected amount of bytes, return 0.
+ * Return: The actual number of bytes the function can read and print, or 0 if:
+ *         - The function fails to open or read the file.
+ *         - `filename` is NULL.
+ *         - `write()` fails or does not write the expected amount of bytes.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, bytes_read, bytes_written;
-	char *buf;
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
-
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
 	{
-		close(fd);
+		free(buffer);
 		return (0);
 	}
 
-	bytes_read = read(fd, buf, letters);
+	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
-		free(buf);
+		free(buffer);
 		close(fd);
 		return (0);
 	}
 
-	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		free(buf);
+		free(buffer);
 		close(fd);
 		return (0);
 	}
 
-	free(buf);
+	free(buffer);
 	close(fd);
 
 	return (bytes_written);
