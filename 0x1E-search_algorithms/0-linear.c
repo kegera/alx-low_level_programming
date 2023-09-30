83cbf80088a16ccb6307db5dda2a4ca1ded06857
kegera <kegeshaddy@gmail.com>
2023-09-30 13:17:00 -0700

    sorting algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,7 +3,7 @@
 /**
  * linear_search - searches for a value in an array of
  * integers using the Linear search algorithm
- *
+ *sorting
  * @array: input array
  * @size: size of the array
  * @value: value to search in
